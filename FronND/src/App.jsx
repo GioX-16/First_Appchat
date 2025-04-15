b972407543331d74d7aee530a278b1d17e5cabea
Geovanny Sandino <sandinowork16@gmail.com>
2025-04-15 10:27:02 -0600

    add styles with semantic react!

--- FronND/src/App.jsx
+++ FronND/src/App.jsx
@@ -1,43 +1,59 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./App.css";
 
-import io from 'socket.io-client';
-import Chat from './Chat'
+import io from "socket.io-client";
+import Chat from "./Chat";
+import { CardContent, Card, Icon, Container, Divider } from "semantic-ui-react";
+import { FormField, Button, Checkbox, Form } from "semantic-ui-react";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [Username, setUsername] = useState("");
-  const [room, setRoom] = useState("")
-
+  const [room, setRoom] = useState("");
 
   const joinRoom = () => {
     if (Username !== "" && room !== "") {
-      socket.emit("join_room", room);   /* AQUI EL USUARIO SE UNE A LA SALA LUEGO DE HABER PUESTO SU USUARIO Y LA SALA */
+      socket.emit(
+        "join_room",
+        room
+      ); /* AQUI EL USUARIO SE UNE A LA SALA LUEGO DE HABER PUESTO SU USUARIO Y LA SALA */
     }
-  }
+  };
 
   return (
     <>
-      <div className="chat">
-        
-        <h1>WELCOME TO GIOX CHAT</h1>
+      <Container>
+        <Card fluid>
+          <CardContent header="WELCOME TO GIOX CHAT" />
+          <CardContent>
+            <Form>
+              <FormField>
+                <label>USERNAME:</label>
+                <input
+                  type="text"
+                  placeholder="username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </FormField>
+              <FormField>
+                <label>ID ROOM:</label>
+                <input
+                  type="text"
+                  placeholder="id room"
+                  onChange={(e) => setRoom(e.target.value)}
+                />
+              </FormField>
 
-        <h3>Join The Room</h3>
-
-        <input
-          type="text"
-          placeholder="USERNAME:"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input type="text" 
-        placeholder="ID ROOM: " 
-        onChange={e => setRoom(e.target.value)}
-        />
-
-        <button onClick={joinRoom}>Join</button>
+              <Button onClick={joinRoom}>Join</Button>
+            </Form>
+          </CardContent>
+          <CardContent extra>
+            <Icon name="user" />8 Friends
+          </CardContent>
+        </Card>
         <Chat socket={socket} username={Username} room={room} />
-      </div>
+      </Container>
     </>
   );
 }
